2235d716b375aecfdd1b93270a0981de5f0f770f
Gervid03 <165380593+Gervid03@users.noreply.github.com>
2024-03-30 19:28:31 +0100

    Replaced Frame with Geo

--- asdasd/Assets/Script/MapEditor.cs
+++ asdasd/Assets/Script/MapEditor.cs
@@ -4,35 +4,39 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
+
 public class MapEditor : MonoBehaviour
 {
     public TileBase basicTile;
     public TileBase clear;
     public List<Tilemap> tilemaps;
     public int currentTilemap;
-    public GameObject ButtonPrefab;
-    public int column;
-    public int row;
+    public float calculatedCellWith;
+    public float calculatedCellHeight;
     public bool add; //false remove
+    public float xBottomLeft, yBottomLeft, xTopRight, yTopRight;
+    public int numberOfCellsInARow;
+    public int numberOfCellsInAColumn;
 
     private void Start()
     {
-        for(int i = 0; i < row; i++)
-        {
-            for(int j = 0; j < column; j++)
-            {
-                GameObject gO = Instantiate(ButtonPrefab, transform);
-                gO.GetComponent<MapEditorButton>().x = j;
-                gO.GetComponent<MapEditorButton>().y = row - i;
-            }
-        }
+        calculatedCellWith = (xTopRight - xBottomLeft) / numberOfCellsInARow;
+        calculatedCellHeight = (yTopRight - xBottomLeft) / numberOfCellsInAColumn;
     }
 
     private void Update()
     {
-
+        HandleClick();
     }
 
+    public void HandleClick()
+    {//     If clicked inside the editor board
+        if (Input.GetMouseButton(0) && Input.mousePosition.x > xBottomLeft && Input.mousePosition.x < xTopRight && Input.mousePosition.y > yBottomLeft && Input.mousePosition.y < yTopRight)
+        {
+            Use(Mathf.FloorToInt((Input.mousePosition.x - xBottomLeft) / calculatedCellWith),
+                Mathf.FloorToInt((Input.mousePosition.y - yBottomLeft) / calculatedCellHeight));
+        }
+    }
     public void AddTile(int x, int y)
     {
         tilemaps[currentTilemap].SetTile(new Vector3Int(x, y, 0), basicTile);
