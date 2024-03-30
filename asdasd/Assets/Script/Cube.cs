581bea3451c8fad049e45837541a89c1a48c030e
Gervid03 <165380593+Gervid03@users.noreply.github.com>
2024-03-30 11:16:27 +0100

    cucc

--- asdasd/Assets/Script/Cube.cs
+++ asdasd/Assets/Script/Cube.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    public int colorIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        FindFirstObjectByType<WallManager>().SubscribeToBeACube(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Teleport();
+    }
+
+    public void Teleport()
+    {
+        if (Input.GetAxisRaw("CubeTP") > 0)
+        {
+
+        }
     }
 }
