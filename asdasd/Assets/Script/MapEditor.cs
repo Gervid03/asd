eb7b1c46e6512b1d6a7e7de6873088b00dc1e1d6
Hora000 <165382360+Hora000@users.noreply.github.com>
2024-03-31 17:34:50 +0200

    mapeditor

--- asdasd/Assets/Script/MapEditor.cs
+++ asdasd/Assets/Script/MapEditor.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
@@ -17,6 +18,15 @@ public class MapEditor : MonoBehaviour
     public float xBottomLeft, yBottomLeft, xTopRight, yTopRight;
     public int numberOfCellsInARow;
     public int numberOfCellsInAColumn;
+    public int currentTool; //0 - remove, 1 - add basic tile, 2 - add button, 3 - add buttonforcube, 4 - add lever, 5 - add portal
+    public List<tool> tools;
+
+    [System.Serializable]
+    public struct tool
+    {
+        public string name;
+        public TileBase tile;
+    }
 
     private void Start()
     {
@@ -29,6 +39,11 @@ public class MapEditor : MonoBehaviour
         HandleClick();
     }
 
+    public void SetTool(int index)
+    {
+        currentTool = index; 
+    }
+
     public void HandleClick()
     {//     If clicked inside the editor board
         if (Input.GetMouseButton(0) && Input.mousePosition.x > xBottomLeft && Input.mousePosition.x < xTopRight && Input.mousePosition.y > yBottomLeft && Input.mousePosition.y < yTopRight)
@@ -37,9 +52,10 @@ public class MapEditor : MonoBehaviour
                 Mathf.FloorToInt((Input.mousePosition.y - yBottomLeft) / calculatedCellHeight));
         }
     }
+
     public void AddTile(int x, int y)
     {
-        tilemaps[currentTilemap].SetTile(new Vector3Int(x, y, 0), basicTile);
+        tilemaps[currentTilemap].SetTile(new Vector3Int(x, y, 0), tools[currentTool].tile);
     }
 
     public void RemoveTile(int x, int y)
@@ -50,7 +66,7 @@ public class MapEditor : MonoBehaviour
     public void Use(int x, int y)
     {
         Debug.Log(x + " " + y);
-        if (add) AddTile(x, y);
-        else RemoveTile(x, y);
+        if (currentTool == 0) RemoveTile(x, y);
+        else AddTile(x, y);
     }
 }
